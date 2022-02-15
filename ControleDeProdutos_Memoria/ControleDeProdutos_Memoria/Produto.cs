f4d03ee9a06f8aeed1f64f767853a5e715ec64f3
rudolfsmoraes <rodolfomsouza@gmail.com>
2022-02-14 21:53:20 -0300

    Projeto Controle de Produtos na Memória

---
+++ ControleDeProdutos_Memoria/ControleDeProdutos_Memoria/Produto.cs
@@ -0,0 +1,8 @@
+﻿public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public int Qtd { get; set; }
+    public string Fabricante { get; set; }
+}
